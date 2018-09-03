fe59766262f8327ee3842265a16e3af44cf9d846
Leevi Lehtonen <leevi.k.lehtonen@gmail.com>
2018-09-03 20:49:19 +0300

    2.4-2.5 Done

--- komponentteja/src/Sisalto.js
+++ komponentteja/src/Sisalto.js
@@ -2,13 +2,10 @@ import React from 'react'
 
 import Osa from './Osa'
 
-const Sisalto = (props) => {
-    const [osa1, osa2, osa3] = props.kurssi.osat
+const Sisalto = ({kurssi}) => {
     return (
         <div>
-            <Osa osa={osa1.nimi} tehtavia={osa1.tehtavia} />
-            <Osa osa={osa2.nimi} tehtavia={osa2.tehtavia} />
-            <Osa osa={osa3.nimi} tehtavia={osa3.tehtavia} />
+            {kurssi.osat.map((osa, id) => <Osa key={id} osa={osa.nimi} tehtavia={osa.tehtavia} />)}
         </div>
     )
 }
