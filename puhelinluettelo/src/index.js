fcea9233675532e3646e755d96160007493919d5
Leevi Lehtonen <leevi.k.lehtonen@gmail.com>
2018-09-04 22:02:14 +0300

    2.6 Done

---
+++ puhelinluettelo/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  handleNewNameChange = (event) => this.setState({newName: event.target.value})
+  addNewName = (event) => {
+	event.preventDefault()
+	const newPersons = this.state.persons.concat({name: this.state.newName})
+	this.setState({persons: newPersons, newName: ''})
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addNewName}>
+          <div>
+            nimi: <input value={this.state.newName} onChange={this.handleNewNameChange}/>
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+		{this.state.persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      </div>
+    )
+  }
+}
+
+export default App
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
